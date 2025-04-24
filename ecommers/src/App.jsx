745b5a78539ffbf411830fc379ffdd9746451fe0
abhimanyuspark <127104226+abhimanyuspark@users.noreply.github.com>
2025-04-24 15:55:33 +0530

    add Loader component and refactor App component for lazy loading and scroll behavior

--- ecommers/src/App.jsx
+++ ecommers/src/App.jsx
@@ -1,24 +1,35 @@
 import "./App.css";
-import BertoozPage from "./components/collection/Collection";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import ProductPage from "./components/display/Display";
-import ProductDetails from "./components/details/Details";
-import { Route, Routes } from "react-router-dom";
+import Loader from "./components/Loader";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+
+const BertoozPage = lazy(() => import("./components/collection/Collection"));
+const ProductPage = lazy(() => import("./components/display/Display"));
+const ProductDetails = lazy(() => import("./components/details/Details"));
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Header />
+
       <Routes>
         <Route path="/" element={<BertoozPage />} />
         <Route path="/product" element={<ProductPage />} />
-        <Route path="/details"element={<ProductDetails />} />
-
+        <Route path="/details" element={<ProductDetails />} />
       </Routes>
 
       <Footer />
-    </>
+    </Suspense>
   );
 }
 
